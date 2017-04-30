9e8f1f718b055cccdd2060b605014fbfdaca8f89
Erik Hanson <pair+erik@citizencode.io>
2017-04-30 01:36:45 -0700

    remove Let's Encrypt since Heroku handles it automatically now

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,4 @@ Rails.application.routes.draw do
 
   resource :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create]
-
-  # SSL/Let's Encrypt
-  get '/.well-known/acme-challenge/:id' => 'acme_challenges#show'
 end
